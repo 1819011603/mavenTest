9a130d7bd120879be10e35f36614b1ff8420dafa
zzl <1819011603@qq.com>
2021-04-13 16:58:25 +0800

    LeetCode建树 泛型的使用Double

--- src/test/java/LeetCode/MinDiffInBST783.java
+++ src/test/java/LeetCode/MinDiffInBST783.java
@@ -46,5 +46,14 @@ public class MinDiffInBST783 {
         trees.preOrder(treeNode);
         trees.inOrder(treeNode);
         trees.behindOrder(treeNode);
+
+        Double[] objects1 = {5.6,0.7,70.1,null,2.0,6.1,null,1.1,3.1,null,8.1,null,null,null,4.0,null,11.1,null,null,9.11,12.0,null,10.0,null,13.1};
+//        Comparable[] objects = {27,null,34,null,58,50,null,44};
+        Trees<Double> trees1 = new Trees<>();
+        TreeNode<Double> treeNode1 = trees1.array2tree( objects1);
+        System.out.println(new MinDiffInBST783().minDiffInBST(treeNode));
+        trees1.preOrder(treeNode1);
+        trees1.inOrder(treeNode1);
+        trees1.behindOrder(treeNode1);
     }
 }
